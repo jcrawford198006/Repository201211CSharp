8c58807dd23a1ddfc1b1b1807a674b1cace55530
Jeffrey S Crawford <jeff.crawford@gmail.com>
2012-11-13 12:37:32 -0500

    ModernMedical Project first Commit
    Modern Medical project first commit of functional code.

---
+++ ModernMedical/ModernMedical/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * User: jeff.crawford
+ * Date: 11/13/2012
+ * Time: 11:48 AM
+ * Modern Medical C# Console Application Example Code.
+ */
+using System;
+
+namespace ModernMedical
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			  int divisableByThree = 0;
+				int divisableByFive = 0;			
+				string result = string.Empty;
+								
+				for(int index = 1; index <= 100; index++)
+				{
+				  divisableByThree = index % 3;		// if index is 3 then divisableByThree is set to 0.
+					divisableByFive = index % 5;		// if index is 5 then divisableByFive is set to 0.
+					
+					if(divisableByThree == 0)
+					{
+						result = "Modern";
+					}					
+				
+					if(divisableByFive == 0)
+					{
+						result = result + "Medical";
+					}
+					
+					if(result == string.Empty)
+					{
+						result = index.ToString();
+					}
+					
+					Console.WriteLine(result);
+					
+					result = string.Empty;
+					
+				}			
+			
+			//Console.ReadKey(true);		// Uncomment for testing.
+		}
+	}
+}
